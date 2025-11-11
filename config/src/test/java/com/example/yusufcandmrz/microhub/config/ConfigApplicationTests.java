3ffb3047eb70258c5eb9bd1a2d95036771b83739
yusufcandmrz <93606208+yusufcandmrz@users.noreply.github.com>
2025-11-11 22:39:54 +0300

    first commit

---
+++ config/src/test/java/com/example/yusufcandmrz/microhub/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yusufcandmrz.microhub.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
