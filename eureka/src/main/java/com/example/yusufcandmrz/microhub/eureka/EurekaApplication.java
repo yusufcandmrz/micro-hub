b206f820099a67eb5001d1e5bce7f4a651a3c87a
yusufcandmrz <93606208+yusufcandmrz@users.noreply.github.com>
2025-11-15 21:45:24 +0300

    Update eureka and gateway service

--- eureka/src/main/java/com/example/yusufcandmrz/microhub/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/example/yusufcandmrz/microhub/eureka/EurekaApplication.java
@@ -2,12 +2,14 @@ package com.example.yusufcandmrz.microhub.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
 
 }
